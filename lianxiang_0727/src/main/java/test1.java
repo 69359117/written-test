9bb4d481e7c26a5622148168d3d125f204e000be
ycr <1012447913@qq.com>
2022-07-27 20:01:08 +0800

    lianxiang-0727

--- lianxiang_0727/src/main/java/test1.java
+++ lianxiang_0727/src/main/java/test1.java
@@ -1,2 +1,15 @@
+import java.util.Scanner;
+
 public class test1 {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+
+
+    }
+
+
+
 }
