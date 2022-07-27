63e080b6051a932a2c9c369433dc21e5ca614818
tandd <13512880869@163.com>
2022-07-27 21:40:39 +0800

    tjh

--- lianxiang_0727/src/main/java/test1.java
+++ lianxiang_0727/src/main/java/test1.java
@@ -1,65 +1,80 @@
+package tjh07;
+
 import java.util.Scanner;
 
-public class test1 {
-
+/**
+ * @author Tdd
+ * @creat 2022-07-27 20:59
+ */
+public class t11 {
     public static void main(String[] args) {
-
         Scanner scanner = new Scanner(System.in);
         int N = scanner.nextInt();
-        int[] nums = new int[N];
-        int[] last = {0, 0};
-        int[] pre = {0, 0};
-        for (int i = 0; i < N; i++) {
-            int color = scanner.nextInt();
-            if(color == 0){
-                nums[i] = 0;
-                last[0] ++;
-            }else {
-                nums[i] = 1;
-                last[1]++;
-            }
+        String[][] temp = new String[N][2];
+        for(int i = 0; i < N; i++){
+            temp[i][0] = scanner.next();
+            temp[i][1] = scanner.next();
         }
 
-        System.out.println(count(nums, pre, last));
+        int start = 0;
+        int sumHour = 0, sumMinus = 0, sumSecond = 0;
 
+
+        while(start < N){
+            while(start < N && temp[start][0].equals("out")) start++;
+            if(start >= N) break;
+
+            //输入时间
+            int inHour = 0;
+            int inMinits = 0;
+            int inSecond = 0;
+            while(start < N && temp[start][0].equals("in")){//输入时间选择最后一次
+                String[] inTime = temp[start][1].split(":");
+                inHour = Integer.valueOf(inTime[0]);
+                inMinits = Integer.valueOf(inTime[1]);
+                inSecond = Integer.valueOf(inTime[2]);
+                start++;
+            }
+            if(start >= N) break;
+
+            //输出时间选第一次的
+            String[] outTime = temp[start][1].split(":");
+            int outHour = Integer.valueOf(outTime[0]);
+            int outMinits = Integer.valueOf(outTime[1]);
+            int outSecond = Integer.valueOf(outTime[2]);
+            start++;
+
+            if(outSecond - inSecond >= 0) sumSecond += outSecond - inSecond;
+            else {
+                sumSecond +=  60 + outSecond - inSecond;
+                outMinits--;
+            }
+            if(outMinits - inMinits >= 0) sumMinus += outMinits - inMinits;
+            else {
+                sumMinus += 60 + outMinits - inMinits;
+                outHour--;
+            }
+            if(outHour - inHour >= 0) sumHour += outHour - inHour;
+            else {
+
+            }
+
+
+
+        }
+        while(sumSecond > 60){
+            sumMinus++;
+            sumSecond-=60;
+        }
+        while(sumMinus > 60){
+            sumHour++;
+            sumMinus-=60;
+        }
+        if(sumHour < 10)
+            System.out.println("0" + sumHour+":"+sumMinus+":"+sumSecond);
+        else
+            System.out.println(sumHour+":"+sumMinus+":"+sumSecond);
     }
-
-
-    public static int count(int[] nums ,  int[] pre ,int[] last){
-        if(last[0] == 0 && last[1]==0){         // 空
-            return 0;
-        }
-
-        int ans = Integer.MAX_VALUE;
-        if(last[0]==0 || last[1] == 0){         // 纯色
-            return 0;
-        }else {
-            ans = Math.min(ans , Math.min(last[0],last[1]));
-        }
-
-        for (int i = 0; i < nums.length; i++) {
-            if(nums[i]==0){
-                last[0]--;
-                pre[0]++;
-            }else {
-                last[1]--;
-                pre[1]++;
-            }
-            int tem = 0;
-            if(last[0]!=0 && last[1] != 0){     // 不是纯色
-                tem += Math.min(ans , Math.min(last[0],last[1]));
-            }
-            if(pre[0]!=0 && pre[1] != 0){
-                tem += Math.min(ans , Math.min(pre[0],pre[1]));
-            }
-            ans = Math.min(ans,tem);
-
-        }
-
-        return ans;
-
-    }
-
 
 
 }
