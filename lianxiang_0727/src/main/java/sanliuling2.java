f7052cab015008afb6eafd60a1e753846df282f3
ycr <1012447913@qq.com>
2022-08-27 16:17:04 +0800

    lianxiang-0727

--- lianxiang_0727/src/main/java/sanliuling2.java
+++ lianxiang_0727/src/main/java/sanliuling2.java
@@ -16,12 +16,38 @@ public class sanliuling2 {
         for(int i = 0; i < nodes.length; i++){
             nodes[i] = new Node(path[0][i], path[1][i], path[2][i]);
         }
+        int l = nodes.length;
+        int[][] ints = new int[l+1][l+1];
+        for (int i = 0; i < nodes.length; i++) {
+            int x = nodes[i].a;
+            ints[nodes[i].a][nodes[i].b] = nodes[i].weight;
+            ints[nodes[i].b][nodes[i].a] = nodes[i].weight;
+        }
+        System.out.println(solution(ints));
+
+        System.out.println(1);
 
     }
 
     public static int solution(int[][] Matrix) {
+        int ans = 0;
+        int num = Matrix.length-1;
+        int index = 1;
+
+
+
 
         return 0;
+    }
 
+    static class Node{
+        private int a, b;
+        private int weight;
+
+        public Node(int a, int b, int weight){
+            this.a = a;
+            this.b = b;
+            this.weight = weight;
+        }
     }
 }
