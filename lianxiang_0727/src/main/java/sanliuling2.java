5d541dc94c460c29cf4d187908e78ed0dcb47b12
ycr <1012447913@qq.com>
2022-08-27 16:09:14 +0800

    lianxiang-0727

--- lianxiang_0727/src/main/java/sanliuling2.java
+++ lianxiang_0727/src/main/java/sanliuling2.java
@@ -3,9 +3,19 @@ import java.util.Scanner;
 public class sanliuling2 {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int N = scanner.nextInt();
-        int[] nums = new int[N];            // 长度
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt(), m = sc.nextInt();//节点数n和道路数m
+        int[][] path = new int[m][3];
+        for(int i = 0; i < 3; i++){
+            for(int j = 0; j < m; j++){
+                path[i][j] = sc.nextInt();
+            }
+        }
+
+        Node[] nodes = new Node[m];
+        for(int i = 0; i < nodes.length; i++){
+            nodes[i] = new Node(path[0][i], path[1][i], path[2][i]);
+        }
 
     }
 
