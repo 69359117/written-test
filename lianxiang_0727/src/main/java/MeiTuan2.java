7fcfb748123ca85685866ab9d783ead75b62c28e
ycr <1012447913@qq.com>
2022-09-03 10:28:20 +0800

    lianxiang-0727

--- lianxiang_0727/src/main/java/MeiTuan2.java
+++ lianxiang_0727/src/main/java/MeiTuan2.java
@@ -6,11 +6,34 @@ import java.util.*;
 public class MeiTuan2 {
     public static void main(String args[]) {
         Scanner cin = new Scanner(System.in);
-        int a, b;
-        while( cin.hasNextInt() ) {
-            a = cin.nextInt();
-            b = cin.nextInt();
+
+        int n = cin.nextInt();
+        int[] ints = new int[n];
+        for (int i = 0; i < n; i++) {
+            ints[i] = cin.nextInt();
+        }
+        int[] solution = solution(ints);
+
+
+
+    }
+
+    public static int[] solution(int[] ints){
+        int[] res = new int[2];
+        int min1 = Integer.MIN_VALUE, min2 = Integer.MIN_VALUE;
+        for (int i : ints) {
+            if(i<min1){
+                min2 = min1;
+                min1 = i;
+            }else if(i<min2){
+                min2 = i;
+            }else {
+
+            }
+
 
         }
+
+        return res;
     }
 }
