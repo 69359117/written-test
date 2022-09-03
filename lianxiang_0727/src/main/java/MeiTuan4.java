6f1a45233b3a66b75a2fdab0526de8cacb715126
tandd <13512880869@163.com>
2022-09-03 10:39:00 +0800

    11

--- lianxiang_0727/src/main/java/MeiTuan4.java
+++ lianxiang_0727/src/main/java/MeiTuan4.java
@@ -9,35 +9,45 @@ import java.util.Scanner;
 
 public class MeiTuan4 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+        Scanner cin = new Scanner(System.in);
+        int n = cin.nextInt();
         int[][] dp = new int[n][2];
         boolean[][] isHas = new boolean[n][26];
+
         for (int i = 1; i < n; i++) {
-            int num = sc.nextInt() - 1;
+            int num = cin.nextInt() - 1;
             dp[i][0] = num;
             dp[num][1]++;
         }
-        String next = sc.next();
+
+        String next = cin.next();
         for (int i = 0; i < n; i++) {
             isHas[i][next.charAt(i) - 'A'] = true;
         }
+
         Queue<Integer> queue = new ArrayDeque<>();
         for (int i = 0; i < n; i++) {
-            if (dp[i][1] == 0) queue.add(i);
+            if (dp[i][1] == 0){
+                queue.add(i);
+            }
         }
-        //
+
         while (!queue.isEmpty()) {
-            int poll = queue.poll();
-            int num = dp[poll][0];
+            int poll = queue.poll(), num = dp[poll][0];
             dp[num][1]--;
+
             add(isHas[num], isHas[poll]);
-            if (num != 0 && dp[num][1] == 0) queue.add(num);
+            if (num != 0 && dp[num][1] == 0){
+                queue.add(num);
+            }
         }
+
         for (int i = 0; i < n; i++) {
             int count = 0;
             for (int j = 0; j < 26; j++) {
-                if (isHas[i][j]) count++;
+                if (isHas[i][j]){
+                    count++;
+                }
             }
             System.out.print(count + " ");
         }
@@ -45,7 +55,9 @@ public class MeiTuan4 {
 
     private static void add(boolean[] arr1, boolean[] arr2) {
         for (int i = 0; i < 26; i++) {
-            if (arr2[i]) arr1[i] = true;
+            if (arr2[i]){
+                arr1[i] = true;
+            }
         }
     }
 }
