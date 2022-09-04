e17def5398fd3d67fabfa4369428221365a38e02
tandd <13512880869@163.com>
2022-09-04 20:10:15 +0800

    111

--- lianxiang_0727/src/main/java/zj_t/t1_sg.java
+++ lianxiang_0727/src/main/java/zj_t/t1_sg.java
@@ -1,5 +1,7 @@
 package lianxiang_0727.src.main.java.zj_t;
 
+import java.net.Inet4Address;
+import java.util.HashMap;
 import java.util.Scanner;
 
 /**
@@ -21,6 +23,7 @@ import java.util.Scanner;
 public class t1_sg {
     static int N;
     static int M;
+    static HashMap<Integer, Integer> map = new HashMap<>();
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         N = sc.nextInt();//高
@@ -42,12 +45,16 @@ public class t1_sg {
     public static int solution(int[][] score, int n, int m){//n为行数 m为列数
         if(n >= N) return 0;
         if(m < 0 || m >= M) return 0;
+        int point = n * M + m;
+        if(map.containsValue(point)) return map.get(point);//point对应的分数
+
         int ans = 0;
         if(score[n][m] == -1){
             ans += Math.max(solution(score, n + 1, m + 1), solution(score, n + 1, m - 1));
         }else{
             ans += solution(score, n + 1, m) + score[n][m];
         }
+        map.put(point, ans);
         return ans;
     }
 }
---
+++ lianxiang_0727/src/main/java/zj_t/t3.java
@@ -0,0 +1,34 @@
+package lianxiang_0727.src.main.java.zj_t;
+
+import java.util.Scanner;
+
+/**
+ * @author Tdd
+ * @creat 2022-09-04 20:04
+ */
+/*
+0处无法修改
+3
+5 2
+abcda
+01110
+7 2
+abbaccb
+1001001
+3 0
+aab
+101
+输出是
+3
+4
+2
+ */
+public class t3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();//组数
+        while (t > 0) {
+            int n = sc.nextInt();
+        }
+    }
+}
