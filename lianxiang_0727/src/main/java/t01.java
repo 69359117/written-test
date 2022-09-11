2968b35f10271b70605b7f904daab4656306f6aa
tandd <13512880869@163.com>
2022-09-12 00:29:05 +0800

    111

--- lianxiang_0727/src/main/java/t01.java
+++ lianxiang_0727/src/main/java/t01.java
@@ -6,7 +6,8 @@ package lianxiang_0727.src.main.java;
  */
 public class t01 {
     public static void main(String[] args) {
-
+        String[] strs = new String[]{"QWER","1","1","666","QWER"};
+        System.out.println(minDistance(strs, "QWER", "666"));
     }
 
 //    public int minDistance(String[] strs, String str1, String str2){
@@ -20,26 +21,27 @@ public class t01 {
 //    }
 
     //字符串数组中两个字符串的最小距离
-    public static int minDistance(String[] strs,String str1,String str2){
-        if(str1==null||str2==null){
+    public static int minDistance(String[] strs, String str1, String str2) {
+        if (str1 == null || str2 == null) {
             return -1;
         }
-        if(str1.equals(str2)){
+        if (str1.equals(str2)) {
             return 0;
         }
-        int last1=-1;//最近一次str1的位置
-        int last2=-1;//最近一次str2的位置
-        int min=Integer.MAX_VALUE;
-        for(int i=0;i<strs.length;i++){
-            if(strs[i].equals(str1)){//找到了str1，如果找到了last2,则i-last2就是当前str1和左边最近str2的距离
-                min=Math.min(min, last2==-1?min:i-last2);//每次都要比较Min
-                last1=i;
+        int last1 = -1;//最近一次str1的位置
+        int last2 = -1;//最近一次str2的位置
+        int min = Integer.MAX_VALUE;//距离
+        for (int i = 0; i < strs.length; i++) {
+            if (strs[i].equals(str1)) {//找到了str1，如果找到了last2,则i-last2就是当前str1和左边最近str2的距离
+                min = Math.min(min, last2 == -1 ? min : i - last2);//每次都要比较Min
+                last1 = i;
             }
-            if(strs[i].equals(str2)){//找到了str2，如果找到了last1,则i-last1就是当前str2和左边最近str1的距离
-                min=Math.min(min, last1==-1?min:i-last1);
+            if (strs[i].equals(str2)) {//找到了str2，如果找到了last1,则i-last1就是当前str2和左边最近str1的距离
+                min = Math.min(min, last1 == -1 ? min : i - last1);
+                last2 = i;
             }
         }
-        return min==Integer.MAX_VALUE?-1:min;
+        return min == Integer.MAX_VALUE ? -1 : min;
 
     }
 }
--- lianxiang_0727/src/main/java/zhineng/t.java
+++ lianxiang_0727/src/main/java/zhineng/t.java
@@ -1,23 +1,23 @@
-
-package zhineng;
-
-import java.util.*;
-
-//0,0,0,0,0,0,0,0
 //
-
-class t{
-
-    public static void main(String[] args) {
-
-        Scanner sc = new Scanner(System.in);
-
-
-    }
-
-    public static int solution(int[][] grid) {
-
-    }
-
-
-}
+//package zhineng;
+//
+//import java.util.*;
+//
+////0,0,0,0,0,0,0,0
+////
+//
+//class t{
+//
+//    public static void main(String[] args) {
+//
+//        Scanner sc = new Scanner(System.in);
+//
+//
+//    }
+//
+//    public static int solution(int[][] grid) {
+//
+//    }
+//
+//
+//}
