5188863648844d5c8dd74c3b82bffa490434c425
ycr <1012447913@qq.com>
2022-09-11 23:52:07 +0800

    lianxiang-0727

---
+++ lianxiang_0727/src/main/java/zhineng/t1.java
@@ -0,0 +1,27 @@
+
+package zhineng;
+
+import java.util.Scanner;
+
+//0,0,0,0,0,0,0,0
+//
+
+class t1 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+
+    }
+
+    public static int solution(int[][] grid) {
+
+
+
+        return 1;
+
+    }
+
+
+}
---
+++ lianxiang_0727/src/main/java/zhineng/t2.java
@@ -0,0 +1,23 @@
+
+package zhineng;
+
+import java.util.Scanner;
+
+//0,0,0,0,0,0,0,0
+//
+
+class t2 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+
+    }
+
+    public static int solution(int[][] grid) {
+        return 1;
+    }
+
+
+}
