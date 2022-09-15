f99e92d426360202b40639cc4b28917ef12effb2
ycr <1012447913@qq.com>
2022-09-15 20:05:10 +0800

    lianxiang-0727

--- lianxiang_0727/src/main/java/t3.java
+++ lianxiang_0727/src/main/java/t3.java
@@ -16,31 +16,19 @@ public class t3 {
     public static void solution(String s) {
         long ans = 0;
         // false 表示该字母为偶数
-        boolean[] flags = new boolean[26];
-        ArrayList<boolean[]> pres = new ArrayList<>();
-        pres.add(new boolean[26]);
+        int flags = 0;
+//        boolean[] flags = new boolean[26];
+        int[] pres = new int[s.length()+1];
+//        ArrayList<Integer> pres = new ArrayList<>();
+        pres[0] = flags;
         for (int i = 0; i < s.length(); i++) {
-            int now = s.charAt(0)-'a';
-            flags[now] = !flags[now];
-            boolean[] nowFlags = new boolean[26];
-            for (int j = 0; j < 26; j++) {
-                nowFlags[j] = flags[j];
-            }
-            pres.add(nowFlags);
+            int now = s.charAt(i)-'a';
+            flags = flags ^ 1 << (now);
+            pres[i+1] = flags;
             for (int j = 0; j <= i; j++) {
-                boolean[] tem = pres.get(j);
-                int sum = 0;
-                for (int k = 0; k < 26; k++) {
-                    if( tem[k] ^ flags[k] ){
-                        if(sum!=0){
-                            sum++;
-                            break;
-                        }else {
-                            sum++;
-                        }
-                    }
-                }
-                if(sum==1){
+                int tem = pres[j];
+                tem = tem ^ flags;
+                if(tem!=0 && ( tem & (tem-1) ) == 0){
                     ans++;
                 }
             }
